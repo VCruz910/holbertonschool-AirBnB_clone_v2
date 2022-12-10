3ee770461133a32e8fa79aa4249c7794c886c95f
raycast5 <ray.j.colon@gmail.com>
2022-12-10 07:02:53 -0400

    Changed back populate to backref in state

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", back_populates="state", cascade="all, delete")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
